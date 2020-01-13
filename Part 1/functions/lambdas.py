59dd2ed25fcd3bdc4b0d310b945c9b38e8e9ad09
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-01-13 08:21:11 +0200

    lambdas

---
+++ Part 1/functions/lambdas.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 08:09:11 2020
+
+@author: Stefan Draghici
+"""
+
+a=lambda x:x**2
+b=lambda x, y:x+y
+c=lambda x, *args, y, **kwargs:(x, args, y, kwargs)
