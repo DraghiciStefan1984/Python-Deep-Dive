7ba4560e75e56c0bfc130f93003b733af7c529f6
draghicistefan <draghicistefan1984@gmail.com>
2020-05-27 08:13:05 +0300

    optimization done

---
+++ Part 1/vars and memory/optimization2.py
@@ -0,0 +1,19 @@
+a='hello'
+b='hello'
+
+print(hex(id(a)))
+print(hex(id(b)))
+
+
+a='hello world'
+b='hello world'
+
+print(hex(id(a)))
+print(hex(id(b)))
+
+
+a='000 hello world'
+b='000 hello world'
+
+print(hex(id(a)))
+print(hex(id(b)))
