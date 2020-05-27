06b3f5fad42515642448136934cadb9863f85bd7
draghicistefan <draghicistefan1984@gmail.com>
2020-05-27 18:24:08 +0300

    ints done

---
+++ Part 1/numeric types/numbers1.py
@@ -0,0 +1,16 @@
+import fractions
+
+a=fractions.Fraction(22, 7)
+print(a)
+print(float(a))
+print(int(a))
+
+print(int('45678765'))
+print(int('10101011100101', base=2))
+
+print(bin(5765))
+print(oct(5765))
+print(hex(5765))
+
+a=0b101
+print(a)
