348e4dcab30a58a1bcddafb04ce51c136173a107
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2023-06-20 11:29:45 +0300

    equality

---
+++ Part1-Functional/equality.py
@@ -0,0 +1,13 @@
+a = 10
+b = 10
+print(a == b)
+print(a is b)
+print(hex(id(a)))
+print(hex(id(b)))
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+print(x == y)
+print(x is y)
+print(hex(id(x)))
+print(hex(id(y)))
