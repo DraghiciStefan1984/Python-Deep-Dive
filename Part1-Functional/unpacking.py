fa4e512c414cc57a4b88a27478f1adc19ed1ff02
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2023-06-21 10:50:31 +0300

    func start

---
+++ Part1-Functional/unpacking.py
@@ -0,0 +1,19 @@
+a = (1, 2, 3)
+print(type(a))
+
+a = 1, 2, 3
+print(type(a))
+
+(a, b, c) = [1, 2, 3]
+print(a, b, c)
+
+# the python way to swap values
+a, b = 10, 20
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+s = {'p', 'y', 't', 'h', 'o', 'n'}
+print(s)
+
